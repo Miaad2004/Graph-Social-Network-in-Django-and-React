81e34880825e8d7299697f651d62932d225ec438
Miaad <Miaad@MIAAD>
2024-01-29 02:31:42 +0330

    Update API URLs to include new views

--- connection_recommender/api/urls.py
+++ connection_recommender/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import main
+from .views import UserLoginView, UserRegistrationView, UserDeleteView, GetAllUsersView, UserLogoutView, AddConnectionView, RemoveConnectionView, GetAllConnectionsView
 
 urlpatterns = [
-    path("", main)
+    path("login/", UserLoginView.as_view()),
+    path("logout/", UserLogoutView.as_view()),
+    path("register/", UserRegistrationView.as_view()),
+    path("delete_acount/", UserDeleteView.as_view()),
+    path("get_all_users/", GetAllUsersView.as_view()),
+    path("get_connections/", GetAllConnectionsView.as_view()),
+    path("add_connection/", AddConnectionView.as_view()),
+    path("remove_connection/", RemoveConnectionView.as_view())
 ]
