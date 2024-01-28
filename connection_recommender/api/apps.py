d4ef048c572a3c88cf8cf1902726ced313d93a5f
Miaad <Miaad@MIAAD>
2024-01-29 02:12:41 +0330

    Add global graph initialization and connection addition in ready() method

--- connection_recommender/api/apps.py
+++ connection_recommender/api/apps.py
@@ -1,6 +1,21 @@
 from django.apps import AppConfig
-
+from .graph import Graph
 
 class ApiConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "api"
+    global_graph = Graph()
+    
+    def ready(self):
+        from .models import Connection
+        
+        global_graph = ApiConfig.global_graph
+        all_connections = Connection.objects.all()
+
+        for connection in all_connections:
+            global_graph.add_connection(str(connection.user1.id), str(connection.user2.id))
+        
+        print("graphhhhhhhh")
+        print(global_graph)
+        import api.signals
+    
