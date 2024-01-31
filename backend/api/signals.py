5b33d96d98e783cf4378c5070f6235ebdfdba13c
Miaad <Miaad@MIAAD>
2024-01-31 22:08:28 +0330

    Update global graph on connection and user create/delete

--- backend/api/signals.py
+++ backend/api/signals.py
@@ -1,6 +1,6 @@
 from django.db.models.signals import post_save, post_delete, post_migrate
 from django.dispatch import receiver
-from .models import Connection
+from .models import Connection, CustomUser
 from .apps import ApiConfig
 from django.apps import AppConfig
 from .graph import Graph
@@ -9,12 +9,31 @@ from .graph import Graph
 def update_global_graph_on_connection_create(sender, instance, created, **kwargs):
     if created:
         global_graph = ApiConfig.global_graph
-        global_graph.add_connection(instance.user1.id, instance.user2.id)
+        global_graph.add_connection(instance.user1.username, instance.user2.username)
+        #print("graph:")
+        #print(global_graph)
+
 
 @receiver(post_delete, sender=Connection)
 def update_global_graph_on_connection_delete(sender, instance, **kwargs):
     global_graph = ApiConfig.global_graph
-    global_graph.remove_connection(instance.user1.id, instance.user2.id)
+    global_graph.remove_connection(instance.user1.username, instance.user2.username)
+    #print("graph:")
+    #print(global_graph)
+
+@receiver(post_save, sender=CustomUser)
+def update_global_graph_on_user_create(sender, instance, created, **kwargs):
+    if created:
+        global_graph = ApiConfig.global_graph
+        global_graph.add_node(instance.username)
+        #print("graph:")
+        #print(global_graph)
+@receiver(post_delete, sender=CustomUser)
+def update_global_graph_on_user_delete(sender, instance, **kwargs):
+    global_graph = ApiConfig.global_graph
+    global_graph.remove_node(instance.username)
+    #print("graph:")
+    #print(global_graph)
 
 @receiver(post_migrate, sender=ApiConfig)
 def build_global_graph(sender, **kwargs):
