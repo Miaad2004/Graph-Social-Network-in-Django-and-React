efef81e3e59b73a1c72a6a87796c9a8aaf06022a
Miaad <Miaad@MIAAD>
2024-01-31 22:08:11 +0330

    Add methods for calculating shared specialties count and correlation score

--- backend/api/models.py
+++ backend/api/models.py
@@ -12,6 +12,30 @@ class CustomUser(AbstractUser):
     
     def __str__(self):
         return self.username
+    
+    def get_shared_specialties_count(self, other_user):
+        count = 0
+        for specialty in self.specialties:
+            if specialty in other_user.specialties:
+                count += 1
+        return count
+    
+    def get_correlation_score(self, other_user):
+        shared_specialties_count = self.get_shared_specialties_count(other_user)
+        have_shared_field = self.field == other_user.field
+        have_shared_university = self.university == other_user.university
+        have_shared_workplace = self.workplace == other_user.workplace
+        age_difference = abs(self.birthday.year - other_user.birthday.year)
+        
+        if age_difference == 0:
+            age_difference_score = 0  
+        else:
+            age_difference_score = 1 / age_difference
+
+        return 2 * shared_specialties_count + 2 * have_shared_field + \
+               have_shared_university + have_shared_workplace + age_difference_score
+    
+    
 
 
 class Connection(models.Model):
