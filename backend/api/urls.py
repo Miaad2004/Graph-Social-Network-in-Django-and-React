462de75022e4ec01f646af3b868c3bca57a46212
Miaad <Miaad@MIAAD>
2024-01-31 17:09:38 +0330

    Update API URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path("login/", UserLoginView.as_view()),
     path("logout/", UserLogoutView.as_view()),
     path("register/", UserRegistrationView.as_view()),
-    path("get_user/", GetUserInfoView.as_view()),
+    path("get_user/", GetCurrentUserInfoView.as_view()),
     path("delete_account/", UserDeleteView.as_view()),
-    path("get_all_users/", GetAllUsersView.as_view()),
+    path("search/", SearchView.as_view()),
     path("get_connections/", GetAllConnectionsView.as_view()),
     path("add_connection/", AddConnectionView.as_view()),
     path("remove_connection/", RemoveConnectionView.as_view())
